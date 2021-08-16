44128417f8324cee996585bed339c5ecbbf8249b
Jobson Morais de Medeiros <jobson_medeiros@hotmail.com>
2021-08-15 21:09:18 -0300

    Instalado o Vue.js

--- main.js
+++ main.js
@@ -34,3 +34,41 @@ var app = new Vue({ el: '#app', data: { message: 'Olá Vue!'} })
   // No console, informe 
   // app4.todos.push({ text: 'Novo item' }). Você verá um 
   // novo item ser acrescentado dinamicamente à lista.
+
+  // Para permitir aos usuários interagir com o aplicativo, podemos 
+  // usar a diretiva v-on para anexar escutas a eventos (event listeners) 
+  // que invocam métodos em nossas instâncias Vue:
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Olá Vue!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
+
+  // Vue também provê a diretiva v-model, que torna a interligação 
+  // de mão dupla (two-way binding) entre a caixa de texto e o estado 
+  // da aplicação uma moleza:
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Escreva aqui!'
+    }
+  })
+
+// No Vue, um componente é essencialmente uma instância Vue com opções predefinidas. Registrar um componente no Vue é simples:
+// Define um novo componente chamado todo-item
+Vue.component('todo-item', {
+    template: '<li>Isso é um item</li>'
+  })
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      message: 'Escreva aqui!'
+    }
+  })
